74a4de49242dfdc00e186c9177604986dbc55c1e
Amit Yathirajadasan <amit@onceuponabreak.com>
2014-08-17 05:51:15 -0400

    testing gh webhooks

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var hooks = require('./routes/hooks');
 
 var app = express();
 var server = require('http').Server(app);
@@ -26,7 +26,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/hooks', hooks);
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -61,7 +61,6 @@ app.use(function(err, req, res, next) {
 
 io.on('connection', function(socket){
   socket.on('alert message', function(msg){
-    console.log(msg);
     io.emit('alert message', msg);
   });
 });
--- routes/hooks.js
+++ routes/hooks.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res) {
-  res.send('respond with a resource');
+router.get('/gh', function(req, res) {
+  console.log(req.body);
+  res.send('success');
 });
 
 module.exports = router;
